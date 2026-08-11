acad295f64601c725914f9a771426d2f0fa76d87
Werner Vanhala <vanhala64@gmail.com>
2026-08-11 22:10:53 +0300

    Refactoring

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flostoslista/models/shopping_item.dart';
+import "package:flostoslista/screens/list_screen.dart";
+import "package:flostoslista/services/shopping_list_service.dart";
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -9,97 +10,27 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final List<ShoppingItem> items = [
-    ShoppingItem(name: "Maito"),
-    ShoppingItem(name: "Leipä"),
-    ShoppingItem(name: "Munat"),
-  ];
-
-  final TextEditingController _controller = TextEditingController();
-
-  void _addItem(String name) {
-    final trimmedName = name.trim();
-    if (trimmedName.isEmpty) return;
-    setState(() {
-      items.add(ShoppingItem(name: trimmedName));
-    });
-    _controller.clear();
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
+  final ShoppingListService _shoppingListService = ShoppingListService();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Ostoslista")),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Text("Tervetuloa ostoslistaan!", style: TextStyle(fontSize: 24)),
-          Expanded(
-            child: ListView.builder(
-              itemCount: items.length,
-              itemBuilder: (context, index) {
-                final item = items[index];
-
-                return CheckboxListTile(
-                  title: Text(
-                    item.name,
-                    style: TextStyle(
-                      decoration: item.isPurchased
-                          ? TextDecoration.lineThrough
-                          : TextDecoration.none,
-                      color: item.isPurchased
-                          ? Theme.of(context).disabledColor
-                          : null,
-                    ),
-                  ),
-                  value: item.isPurchased,
-                  controlAffinity: ListTileControlAffinity.leading,
-                  secondary: IconButton(
-                    icon: const Icon(Icons.delete),
-                    onPressed: () {
-                      setState(() {
-                        items.removeAt(index);
-                      });
-                    },
-                  ),
-                  onChanged: (value) {
-                    setState(() {
-                      item.isPurchased = value ?? false;
-                    });
-                  },
-                );
-              },
-            ),
-          ),
-          Container(
-            alignment: .bottomCenter,
-            padding: const .all(6),
-            child: TextFormField(
-              controller: _controller,
-              onFieldSubmitted: (value) {
-                _addItem(value);
-              },
-              decoration: InputDecoration(
-                suffixIcon: ElevatedButton(
-                  onPressed: () {
-                    _addItem(_controller.text);
-                  },
-                  child: const Text("Lisää"),
-                ),
-                hintText: "Lisää ostos...",
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8),
-                ),
+      appBar: AppBar(title: const Text("Villkommen!")),
+      body: Align(
+        alignment: Alignment(0, -0.75),
+        child: ElevatedButton(
+          //TÄHÄN VOI LAITTAA COLUMNIN JOS HALUAA USEITA NAPPEJA
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) =>
+                    ListScreen(shoppingListService: _shoppingListService),
               ),
-            ),
-          ),
-        ],
+            );
+          },
+          child: const Text("Avaa ostoslista"),
+        ),
       ),
     );
   }
---
+++ lib/screens/list_screen.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:flostoslista/services/shopping_list_service.dart';
+
+class ListScreen extends StatefulWidget {
+  final ShoppingListService shoppingListService;
+  const ListScreen({super.key, required this.shoppingListService});
+
+  @override
+  State<ListScreen> createState() => _ListScreenState();
+}
+
+class _ListScreenState extends State<ListScreen> {
+  final TextEditingController _controller = TextEditingController();
+
+  void _addItem(String name) {
+    setState(() {
+      widget.shoppingListService.addItem(name);
+      _controller.clear();
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Ostoslista")),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Text("Tervetuloa ostoslistaan!", style: TextStyle(fontSize: 24)),
+          Expanded(
+            child: ListView.builder(
+              itemCount: widget.shoppingListService.items.length,
+              itemBuilder: (context, index) {
+                final item = widget.shoppingListService.items[index];
+
+                return CheckboxListTile(
+                  title: Text(
+                    item.name,
+                    style: TextStyle(
+                      decoration: item.isPurchased
+                          ? TextDecoration.lineThrough
+                          : TextDecoration.none,
+                      color: item.isPurchased
+                          ? Theme.of(context).disabledColor
+                          : null,
+                    ),
+                  ),
+                  value: item.isPurchased,
+                  controlAffinity: ListTileControlAffinity.leading,
+                  secondary: IconButton(
+                    icon: const Icon(Icons.delete),
+                    onPressed: () {
+                      setState(() {
+                        widget.shoppingListService.removeItem(index);
+                      });
+                    },
+                  ),
+                  onChanged: (value) {
+                    setState(() {
+                      widget.shoppingListService.updatePurchased(
+                        index,
+                        value ?? false,
+                      );
+                    });
+                  },
+                );
+              },
+            ),
+          ),
+          Container(
+            alignment: .bottomCenter,
+            padding: const .all(6),
+            child: TextFormField(
+              controller: _controller,
+              onFieldSubmitted: (value) {
+                _addItem(value);
+              },
+              decoration: InputDecoration(
+                suffixIcon: ElevatedButton(
+                  onPressed: () {
+                    _addItem(_controller.text);
+                  },
+                  child: const Text("Lisää"),
+                ),
+                hintText: "Lisää ostos...",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
